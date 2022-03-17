4bc24022a12d2496804a24e318d7fe5e0e4ef9cf
ZAFER <ZAFER@ZAFERAVCI>
2022-03-17 16:55:08 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    //SOLID
+    //Open Closed Principle
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var item in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(item.ProductName);              
+            }
+
+            
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            ///oracle ,Sql,postgres,MongoDb
+            _products = new List<Product> {
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",Unitprice=15,UnitsInStock=15},
+                new Product{ProductId=3,CategoryId=1,ProductName="Klavye",Unitprice=15,UnitsInStock=3},
+                new Product{ProductId=4,CategoryId=2,ProductName="Mause",Unitprice=15,UnitsInStock=2},
+                new Product{ProductId=5,CategoryId=2,ProductName="Kalem",Unitprice=15,UnitsInStock=65},
+                new Product{ProductId=6,CategoryId=1,ProductName="Monitor",Unitprice=15,UnitsInStock=1},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ - Language Integreted Query 
+            //Lambda
+            //SingleOrDefault Foreach yap demek
+
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //göderdiğim ürün ID sine sahip olan listedeki ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.Unitprice = product.Unitprice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+    }
+}
